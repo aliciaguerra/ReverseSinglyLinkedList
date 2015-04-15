06162b9bf11e73162fe2682068ac003191f61c87
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-15 01:00:37 -0700

    Create reverselistiteratively.java

---
+++ reverselistiteratively.java
@@ -0,0 +1,31 @@
+public reverseListIteratively (Node head) 
+{
+if (head==NULL || head.next==NULL)
+return; //empty or just one node in list
+
+Node Second=head.next;
+
+//store third node before we change
+Node Third=Second.next;
+
+//Second's next pointer
+Second.next=head; //second now points to head
+head.next=NULL; //change head pointer to NULL
+
+//only two nodes which we already reversed
+if(Third=NULL)
+return;
+
+Node CurrentNode = Third;
+Node PreviousNode = Second;
+
+while (CurrentNode!=NULL)
+{
+Node NextNode = CurrentNode.next;
+CurrentNode.next = PreviousNode;
+//repeat the process, but have to reset the currentNode and NextNode
+PreviousNode=CurrentNode;
+CurrentNode=NextNode;
+}
+head = PreviousNode; //reset the head node
+}
